55d211d34e982ec26896ea65cff9b3ce850b9e6a
Antonio Ruggia Piquer <149959739+kaem0n@users.noreply.github.com>
2024-05-09 15:42:57 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-icons/font/bootstrap-icons.min.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes></Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
