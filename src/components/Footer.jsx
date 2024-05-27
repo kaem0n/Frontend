eb515e5b56f5918ab0792cac3c3310ac7fda0f74
Antonio Ruggia Piquer <149959739+kaem0n@users.noreply.github.com>
2024-05-27 18:28:51 +0200

    updates main footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,27 @@
 const Footer = () => {
   return (
     <p className="text-center py-3 fs-7">
-      &copy;{new Date().getFullYear()} kaem0n
+      MeeToo &copy; {new Date().getFullYear()} by kaem0n ·{' '}
+      <a
+        href="https://github.com/kaem0n"
+        className="link-body-emphasis underline"
+      >
+        GitHub
+      </a>{' '}
+      ·{' '}
+      <a
+        href="https://www.linkedin.com/in/antonio-ruggia-piquer-a086592b5/"
+        className="link-body-emphasis underline"
+      >
+        LinkedIn
+      </a>{' '}
+      ·{' '}
+      <a
+        href="https://www.instagram.com/_antoniorp_/"
+        className="link-body-emphasis underline"
+      >
+        Instagram
+      </a>
     </p>
   )
 }
